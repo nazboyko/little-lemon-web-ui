bea90b3ddadd9c7df890e313a2a57886de445c5f
Nazar <nazar.boiko@mindgeekconsultants.com>
2024-07-06 01:33:41 -0500

    adding new info block

--- src/Componencts/Main.js
+++ src/Componencts/Main.js
@@ -1,10 +1,24 @@
 // Main.js
 import React from 'react';
+import '../assets/styles/Main.css';
+import yourImage from '../assets/images/restauranfood.jpg'; // Import your image
 
 function Main() {
   return (
-    <main>
-      {/* Add main content here */}
+    <main className="main-content">
+      <div className="page-top-container">
+        <div className="info-block">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          </p>
+          <button className="reserve-button">Reserve a Table</button>
+        </div>
+        <div className="image-block">
+          <img src={yourImage} alt="Restaurant" />
+        </div>
+      </div>
     </main>
   );
 }
