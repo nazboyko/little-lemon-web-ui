d6e0f6820f4d9b6b5e085b1e068205653e9fce69
Nazar <nazar.boiko@mindgeekconsultants.com>
2024-07-10 20:52:41 -0500

    adding About component

--- src/Componencts/Main/About.js
+++ src/Componencts/Main/About.js
@@ -1,18 +1,27 @@
-// About.js
 import React from 'react';
 import '../../assets/styles/Main/About.css';
+import aboutImage1 from '../../assets/images/Mario_and_Adrian.jpg'; // Add your image path
+import aboutImage2 from '../../assets/images/restaurant_chef.jpg'; // Add your image path
 
 function About() {
   return (
     <section className="about">
-      <h1>Little Lemon</h1>
-      <h2>Chicago</h2>
-      <p>
-        We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
-      </p>
-      <p>
-        At Little Lemon, our passion is to embrace the timeless flavors of the Mediterranean, infusing them with a modern twist. We're a family-owned establishment dedicated to bringing you the heartwarming traditions of our heritage, one delectable dish at a time.
-      </p>
+      <div className="about-container">
+        <div className="about-text">
+          <h1>Little Lemon</h1>
+          <h2>Chicago</h2>
+          <p>
+            We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          </p>
+          <p>
+            At Little Lemon, our passion is to embrace the timeless flavors of the Mediterranean, infusing them with a modern twist. We're a family-owned establishment dedicated to bringing you the heartwarming traditions of our heritage, one delectable dish at a time.
+          </p>
+        </div>
+        <div className="about-images">
+          <img src={aboutImage1} alt="About Us" className="about-image" />
+          <img src={aboutImage2} alt="Our Story" className="about-image" />
+        </div>
+      </div>
     </section>
   );
 }
