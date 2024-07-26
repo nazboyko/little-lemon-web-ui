2530376a1afab4baa25dfce4b1845f0899cbc318
Nazar <nazar.boiko@mindgeekconsultants.com>
2024-07-26 15:45:16 -0500

    adding simple info block about this test (not real) example website

---
+++ src/Components/Main/Announcement.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../assets/styles/Main/Announcement.css';
+
+function Announcement() {
+  return (
+    <div className="announcement">
+      <p>
+        This website is a test assignment for <a href="https://www.coursera.org/professional-certificates/meta-front-end-developer" target="_blank" rel="noopener noreferrer">Front-end Developer Specialization by Meta</a>.
+      </p>
+    </div>
+  );
+}
+
+export default Announcement;
